5eafecab9a969f8dab586ae26fa2ad85eebf3d72
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-08-06 23:14:20 -0500

    front end integrated

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const { type } = require('os');
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-
+//makes it so front end resources are added when another is request(like css and js front end files)
+app.use(express.static('public'));
 
 
 
@@ -104,15 +105,27 @@ app.get('/api/animals/:id', (req, res) => {
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
-  
+
     // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
-      res.status(400).send('The animal is not properly formatted.');
+        res.status(400).send('The animal is not properly formatted.');
     } else {
-      const animal = createNewAnimal(req.body, animals);
-      res.json(animal);
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
     }
-  });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
